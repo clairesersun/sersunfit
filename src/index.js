81516d17ad2c2bf2a3cfdc7d6b56e357139cc030
Claire Sersun <claire.sersun@gmail.com>
2025-12-20 08:31:41 -0500

    Remove react-snap to fix Vercel build

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@
  * ============================================
  * REACT ENTRY POINT
  * ============================================
- * 
+ *
  * This file bootstraps the React application.
  * It mounts the App component to the DOM.
- * 
+ *
  * ============================================
  */
 
@@ -13,23 +13,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-
-// Use hydrate if pre-rendered by react-snap, otherwise render normally
-if (rootElement.hasChildNodes()) {
-  // Hydrate pre-rendered content
-  ReactDOM.hydrateRoot(
-    rootElement,
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  // Normal render for development
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
